7244f7199bc4ddfe585fa25792613e0b00bdc4fe
Niloofar <niloo.dehdari@gmail.com>
2026-02-28 01:10:44 +0000

    change to see if ghactions works

--- script.r
+++ script.r
@@ -3,19 +3,39 @@ library(rvest)
 library(git2r)
 library(renv)
 
-repo_url <- "https://github.com/V2RAYCONFIGSPOOL/V2RAY_SUB"
-local_path <- "repo"
+repo_url <- 
+  "https://github.com/V2RAYCONFIGSPOOL/V2RAY_SUB"
+
+local_path <-
+  "repo"
 
 if (!dir.exists(local_path)) {
+  
   git2r::clone(url = repo_url, local_path = local_path)
+  
 } else {
-  r <- git2r::repository(local_path)
+  
+  r <- 
+    git2r::repository(local_path)
+  
   git2r::pull(r)
+  
 }
 
-dirs <- list.files(local_path)
-last_date <- dirs %>% parse_date() %>% na.omit() %>% max()
-last_file <- list.files(file.path(local_path, last_date)) %>% max()
+dirs <-
+  list.files(local_path)
+
+last_date <-
+  dirs %>% 
+  parse_date() %>%
+  na.omit() %>%
+  max()
+
+last_file <-
+  list.files(
+    file.path(local_path, last_date)
+    ) %>%
+  max()
 
 file.copy(
   file.path(local_path, last_date, last_file),
